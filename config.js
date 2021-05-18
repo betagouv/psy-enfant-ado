c2eab21abcd1e67dd7ecfe457abc7f2a187bf93c
carolineBda <caroline.bourdeu@gmail.com>
2021-05-18 12:17:40 +0200

    :sparkles: add link to form

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ module.exports = {
 
   port: process.env.PORT || 8080,
   contactEmail: process.env.CONTACT_EMAIL || 'psyenfantado@beta.gouv.fr',
+  demarchesSimplifieesUrl: process.env.DEMARCHES_SIMPLIFIEES_URL,
   secret: process.env.SECRET || 'Angonyx meeki is a moth of the family Sphingidae'
 };
