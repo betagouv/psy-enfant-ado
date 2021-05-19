ddffbc87a579b48844449a1c6a11b9702d15df46
Paul Leclercq <paleclercq@gmail.com>
2021-05-19 10:53:25 +0200

    linter

--- cron_jobs/cronDemarchesSimplifiees.js
+++ cron_jobs/cronDemarchesSimplifiees.js
@@ -1,11 +1,8 @@
 require('dotenv').config();
 
-const ejs = require('ejs');
 const dbsApiCursor = require('../db/dsApiCursor');
 const dbPsychologists = require('../db/psychologists');
 const demarchesSimplifiees = require('../services/demarchesSimplifiees');
-const config = require('../services/config');
-const emailUtils = require('../services/email');
 
 /**
  * Some data can be modified after been loaded inside PG
@@ -44,29 +41,3 @@ module.exports.importEveryDataFromDSToPG = async function importEveryDataFromDST
 module.exports.importLatestDataFromDSToPG = async function importLatestDataFromDSToPG() {
   importDataFromDSToPG(false);
 };
-
-const sendAlertEmail = async function sendAlertEmail(badPsychologists) {
-  try {
-    const html = await ejs.renderFile('./views/emails/multipleAcceptedAlert.ejs', {
-      badPsychologists,
-      hostnameWithProtocol: config.hostnameWithProtocol,
-    });
-    await emailUtils.sendMail(config.teamEmail, 'Dossiers multiples détéctés !', html);
-    console.debug('Email sent for multiple accounts alert');
-  } catch (err) {
-    console.error("Erreur d'envoi de mail, le mail d'alerte n'est pas envoyé.", err);
-  }
-};
-
-// One person should not have multiple dossiers in "acepte" status, notify the team.
-const checkForMultipleAcceptedDossiers = async () => {
-  const count = await dbPsychologists.countAcceptedPsychologistsByPersonalEmail();
-  const badPsychologists = count.filter((statsPoint) => statsPoint.count > 1);
-  if (badPsychologists.length > 0) {
-    console.log('Detected psychologists with multiple accepted dossiers!', badPsychologists);
-    await sendAlertEmail(badPsychologists);
-  }
-
-  return true;
-};
-module.exports.checkForMultipleAcceptedDossiers = checkForMultipleAcceptedDossiers;
