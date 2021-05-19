58508cdd16153801c9951fa56d73a89df4bb5a60
Paul Leclercq <paleclercq@gmail.com>
2021-05-19 16:55:25 +0200

    use .env file to protect secret

--- config.js
+++ config.js
@@ -5,11 +5,12 @@ module.exports = {
   port: process.env.PORT || 8080,
   contactEmail: process.env.CONTACT_EMAIL || 'psyenfantado@beta.gouv.fr',
   databaseUrl: process.env.DATABASE_URL || 'postgres://db:db@localhost:5432/db',
-  uuidNamespace: process.env.UUID_NAMESPACE || "979fcd81-2bfd-4640-9ac0-77ed1033ae60", // used to generate uuid
+  uuidNamespace: process.env.UUID_NAMESPACE || "979fcd81-2bfd-4640-9ac0-77ed1033ae60", // don't use default
   sentryDNS: process.env.SENTRY_DNS || false,
-  demarchesSimplifieesUrl: process.env.DEMARCHES_SIMPLIFIEES_URL,
   demarchesSimplifieesId: process.env.DEMARCHES_SIMPLIFIEES_ID,
-  demarchesSimplifieesIdApiUrl: process.env.DEMARCHES_SIMPLIFIEES_API_URL,
+  // eslint-disable-next-line max-len
+  demarchesSimplifieesIdApiUrl: process.env.DEMARCHES_SIMPLIFIEES_API_URL  || 'https://www.demarches-simplifiees.fr/api/v2/graphql',
   demarchesSimplifieesIdApiToken: process.env.DEMARCHES_SIMPLIFIEES_API_TOKEN,
+  demarchesSimplifieesUrl: process.env.DEMARCHES_SIMPLIFIEES_URL,
   secret: process.env.SECRET || 'Angonyx meeki is a moth of the family Sphingidae'
 };
