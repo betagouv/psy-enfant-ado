a9446f406c96a15fd66a40f1ea5ec9f1ff266bdc
carolineBda <caroline.bourdeu@gmail.com>
2021-05-28 11:30:58 +0200

    Fix file corrupted after merge

--- test/seed/fake_data.js
+++ test/seed/fake_data.js
@@ -9,13 +9,13 @@ exports.seed = async function seed(knex) {
   const numberOfPsyToCreate = 100;
 
   const randomAcceptedPsy = Array(numberOfPsyToCreate).fill(0).map(
-    () => clean.getOnePsy(`${clean.getRandomInt()}@beta.gouv.fr`, demarchesSimplifiees.DOSSIER_STATE.accepte, false),
+    () => clean.getOnePsy(demarchesSimplifiees.DOSSIER_STATE.accepte, false),
   );
 
   const psyWithDifferentStateArchivedStatus = [
-    clean.getOnePsy('archived@beta.gouv.fr', demarchesSimplifiees.DOSSIER_STATE.accepte, true),
-    clean.getOnePsy('construction@beta.gouv.fr', demarchesSimplifiees.DOSSIER_STATE.en_construction, false),
-    clean.getOnePsy('refuse@beta.gouv.fr', demarchesSimplifiees.DOSSIER_STATE.refuse, false),
+    clean.getOnePsy(demarchesSimplifiees.DOSSIER_STATE.accepte, true),
+    clean.getOnePsy(demarchesSimplifiees.DOSSIER_STATE.en_construction, false),
+    clean.getOnePsy(demarchesSimplifiees.DOSSIER_STATE.refuse, false),
   ];
 
   const psyList = psyWithDifferentStateArchivedStatus.concat(randomAcceptedPsy);
