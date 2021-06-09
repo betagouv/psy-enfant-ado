47fa953a82bcbeaccc92723c3c95bc92755903b7
carolineBda <caroline.bourdeu@gmail.com>
2021-06-09 19:09:20 +0200

    Try to fix tracker

--- services/helmet.js
+++ services/helmet.js
@@ -4,7 +4,9 @@ const helmet = require('helmet');
 module.exports = helmet.contentSecurityPolicy({
   directives: {
     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-    'script-src': ["'self'", 'https://stats.data.gouv.fr/', `'nonce-${config.nonceId}'`],
+    'script-src': [
+      "'self'", 'https://stats.data.gouv.fr/', `'nonce-${config.nonceId}'`, 'https://www.googletagmanager.com'
+    ],
     'script-src-attr': ["'unsafe-inline'"],
     'img-src': ["'self'", 'https://stats.data.gouv.fr/', 'data:'],
     'frame-src': ["'self'", 'https://stats.data.gouv.fr/', 'https://*.fls.doubleclick.net/']
--- static/js/gtm-setup.js
+++ static/js/gtm-setup.js
@@ -2,4 +2,3 @@ window.dataLayer = window.dataLayer || [];
 function gtag () {dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'DC-2953234');
-gtag('event', 'conversion', { 'allow_custom_scripts': true, 'send_to': 'DC-2953234/SIG-M0/lppsyenf+unique' });
