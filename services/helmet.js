d351d2bfb226f09b26d4ec11764634b000fe3168
carolineBda <caroline.bourdeu@gmail.com>
2021-06-11 09:50:58 +0200

    clean-up

--- services/helmet.js
+++ services/helmet.js
@@ -5,7 +5,7 @@ module.exports = helmet.contentSecurityPolicy({
   directives: {
     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
     'script-src': [
-      "'self'", 'https://stats.data.gouv.fr/', `'nonce-${config.nonceId}'`, 'https://www.googletagmanager.com'
+      "'self'", 'https://stats.data.gouv.fr/',  'https://www.googletagmanager.com'
     ],
     'script-src-attr': ["'unsafe-inline'"],
     'img-src': ["'self'", 'https://stats.data.gouv.fr/', 'data:'],
