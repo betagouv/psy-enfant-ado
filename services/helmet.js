610098ada1b9e3e216f68dc0a737e77ad18d4567
Caroline <4971715+carolineBda@users.noreply.github.com>
2021-06-16 12:02:15 +0200

    fix js error in console, and remove un-used js import (#25)

--- services/helmet.js
+++ services/helmet.js
@@ -3,9 +3,9 @@ const helmet = require('helmet');
 module.exports = helmet.contentSecurityPolicy({
   directives: {
     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-    'script-src': ["'self'", 'https://stats.data.gouv.fr/',  'https://www.googletagmanager.com'],
+    'script-src': ["'self'", 'https://stats.data.gouv.fr/', 'https://www.googletagmanager.com'],
     'script-src-attr': ["'unsafe-inline'"],
-    'img-src': ["'self'", 'https://stats.data.gouv.fr/', 'data:'],
+    'img-src': ["'self'", 'https://stats.data.gouv.fr/', 'data:', 'https://*.fls.doubleclick.net/'],
     'frame-src': ["'self'", 'https://stats.data.gouv.fr/', 'https://*.fls.doubleclick.net/']
   },
 });
