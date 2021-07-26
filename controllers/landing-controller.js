7bb52d1cd9883444362764736c933907dd18ebc7
Caroline <4971715+carolineBda@users.noreply.github.com>
2021-07-26 09:47:56 +0200

    ✨ add 2 new landing pages + update home (#32)

--- controllers/landing-controller.js
+++ controllers/landing-controller.js
@@ -1,7 +1,13 @@
 const config = require('../config');
 
-module.exports.getLanding = async function getLanding(req, res) {
-  const formUrl = config.demarchesSimplifieesUrl;
+const formUrl = config.demarchesSimplifieesUrl;
+
+module.exports.getLanding = async function getLanding (req, res) {
 
   res.render('landing', { formUrl, config });
+};
+
+module.exports.getPsy = async function getPsy (req, res) {
+
+  res.render('psy', { formUrl, config });
 };
