7d0d0e4b841c9b0db6c89286a810878340cf389e
carolineBda <caroline.bourdeu@gmail.com>
2021-11-25 12:43:49 +0100

    Add email banner

--- services/helmet.js
+++ services/helmet.js
@@ -3,7 +3,8 @@ const helmet = require('helmet');
 module.exports = helmet.contentSecurityPolicy({
   directives: {
     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-    'script-src': ["'self'", 'https://stats.data.gouv.fr/'],
-    'img-src': ["'self'", 'https://stats.data.gouv.fr/', 'data:']
+    'script-src': ["'self'", 'https://stats.data.gouv.fr/', 'https://forms.sbc08.com', "'unsafe-inline'"],
+    'img-src': ["'self'", 'https://stats.data.gouv.fr/', 'data:', 'https://forms.sbc08.com'],
+    'default-src': ["'self'", 'https://stats.data.gouv.fr/', 'https://services.sarbacane.com']
   },
 });
