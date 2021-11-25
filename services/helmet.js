4c20de9b27479d0f27e425e4f50f0c84fefa143a
carolineBda <caroline.bourdeu@gmail.com>
2021-11-25 13:22:38 +0100

    Add données perso

--- services/helmet.js
+++ services/helmet.js
@@ -5,6 +5,7 @@ module.exports = helmet.contentSecurityPolicy({
     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
     'script-src': ["'self'", 'https://stats.data.gouv.fr/', 'https://forms.sbc08.com', "'unsafe-inline'"],
     'img-src': ["'self'", 'https://stats.data.gouv.fr/', 'data:', 'https://forms.sbc08.com'],
-    'default-src': ["'self'", 'https://stats.data.gouv.fr/', 'https://services.sarbacane.com']
+    'default-src': ["'self'", 'https://stats.data.gouv.fr/', 'https://services.sarbacane.com'],
+    'script-src-attr': ["'unsafe-inline'"]
   },
 });
