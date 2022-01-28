1df232a9918d7723cf5a49fcf78a11bff917f5fa
carolineBda <caroline.bourdeu@gmail.com>
2022-01-28 15:00:44 +0100

    Feedback laetitia

--- services/faq/pricing.js
+++ services/faq/pricing.js
@@ -3,8 +3,8 @@ module.exports = [
   {
     q: 'Comment se passe le remboursement ? Dans quel délai ?',
     r:
-      'Après validation de votre participation au dispositif, vous recevrez la convention signée par l’Assurance Maladie, ainsi que les modalités de facturation, avec notamment la feuille de soins spécifique à ce parcours. Après la dernière séance de prise en charge, vous transmettez directement la prescription du médecin et la feuille de soins à votre Caisse Primaire d’Assurance Maladie de rattachement. L’envoi des feuilles de soins à la caisse va déclencher les opérations de paiement. Le paiement intervient dans les jours qui suivent la réception de la feuille de soins et de l’ordonnance<br/>'
-      + 'Dans le cadre de ce dispositif, les séances réalisées après le 31 janvier 2022 ne pourront donner lieu à aucun remboursement.',
+      'Après la dernière séance de prise en charge, vous transmettez directement la prescription du médecin et la feuille de soins à votre Caisse Primaire d’Assurance Maladie de rattachement. L’envoi des feuilles de soins à la caisse va déclencher les opérations de paiement. Le paiement intervient dans les jours qui suivent la réception de la feuille de soins et de l’ordonnance<br/>'
+     + 'Dans le cadre de ce dispositif, les séances réalisées après le 31 janvier 2022 ne pourront donner lieu à aucun remboursement.',
   },
   {
     q: 'Concrètement, comment remplir la feuille de soins à envoyer à la CPAM pour remboursement ?',
