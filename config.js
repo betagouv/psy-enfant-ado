952935729270630f8a3d4a8941bb6c26b3e4571c
Caroline <4971715+carolineBda@users.noreply.github.com>
2022-02-01 10:28:29 +0100

    :fire: close dispositif ! (#38)

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ module.exports = {
   appName: 'Psy Enfant Ado',
   appDescription: 'En parler, c’est déjà se soigner. Faites bénéficier votre enfant d’un accompagnement psychologique gratuit.',
   port: process.env.PORT || 8080,
-  contactEmail: process.env.CONTACT_EMAIL || 'contact-psyenfantado@beta.gouv.fr',
   databaseUrl: process.env.DATABASE_URL || 'postgres://db:db@localhost:5432/db',
   uuidNamespace: process.env.UUID_NAMESPACE || '979fcd81-2bfd-4640-9ac0-77ed1033ae60', // don't use default
   demarchesSimplifieesId: process.env.DEMARCHES_SIMPLIFIEES_ID,
