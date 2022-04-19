ff32f6bef6b1df324a5ac49f46df16f60b685001
Caroline <4971715+carolineBda@users.noreply.github.com>
2022-04-19 17:41:23 +0200

    remove faq + update stats on home (#42)

--- services/faq/general.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable max-len */
-module.exports = [
-  {
-    q: 'Quel est l’objet du dispositif ?',
-    r: 'La crise sanitaire a dégradé la santé mentale, et plus particulièrement celle des enfants et adolescents. Afin d’apporter une réponse rapide à cette dégradation, le Président de la République, en visite au CHU de Reims le 14 avril 2021, a annoncé la création d’un dispositif de soutien psychologique d’urgence d’envergure nationale. Ce dispositif PsyEnfantAdo s’adresse aux jeunes de 3 à 17 ans inclus, en souffrance psychologique d’intensité légère à modérée. Il prévoit ainsi pour chaque enfant ou adolescent, en fonction de ses besoins, une prise en charge psychologique chez le psychologue dans la limite de 10 séances par jeune. <br/>'
-      + 'Ce soutien psychologique d’urgence peut être mobilisé par les médecins pour répondre aux besoins des enfants et adolescents jusqu’à la fin de l’année. Le suivi psychologique initié dans ce cadre s’achèvera au plus tard le 31 janvier 2022. Il est donc recommandé de commencer le suivi le plus tôt possible, pour échelonner le suivi psychologique jusqu’à cette date.',
-  },
-];
